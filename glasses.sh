29acf2a0a435467b21d6ab0a34363340c2cff4c1
Edward Chou <ejchou2@illinois.edu>
2017-11-22 14:21:30 -0800

    changed directory

--- glasses.sh
+++ glasses.sh
@@ -1,5 +1,5 @@
 # CHANGE THIS FOR YOU
-declare -r path_FirstAid=/Users/yidarvin/Desktop/FirstAid/train_CNNclassification.py
+declare -r path_FirstAid=../FirstAid/train_CNNclassification.py
 
 declare -r path_train=$PWD/h5_data/training
 declare -r path_val=$PWD/h5_data/testing
@@ -10,4 +10,4 @@ declare -r path_model=$PWD/model_state/$name.ckpt
 declare -r path_log=$PWD/logs/$name.txt
 declare -r path_vis=$PWD/graphs
 
-python $path_FirstAid --pTrain $path_train --pVal $path_val --name $name --pModel $path_model --pLog $path_log --pVis $path_vis --nGPU 0 --bs 8 --ep 50 --nClass 2 --lr 0.001 --do 0.5
+python2 $path_FirstAid --pTrain $path_train --pVal $path_val --name $name --pModel $path_model --pLog $path_log --pVis $path_vis --nGPU 0 --bs 8 --ep 50 --nClass 2 --lr 0.001 --do 0.5
